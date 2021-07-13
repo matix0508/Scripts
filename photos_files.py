0683d49c6df96e949fa35e9d9718ec79cfa76765
Mateusz Wlekliński <matix0508@gmail.com>
2021-07-13 02:44:35 +0200

    optimize

--- photos_files.py
+++ photos_files.py
@@ -1,15 +1,7 @@
-import os
-import shutil
-from typing import List
 from move_files import MoveApp
 
 
 PHOTO_DIR = '/home/wleklinski/Pictures'
-SCREENSHOTS_DIR = os.path.join(PHOTO_DIR, 'Screenshots')
-MEMES_DIR = os.path.join(PHOTO_DIR, 'Memes')
-
-TARGET_DIRS = [SCREENSHOTS_DIR, MEMES_DIR]
-
 CATEGORIES = ['screenshot', 'meme']
 
 
