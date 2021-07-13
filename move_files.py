34ca56f58183e7696e01c2f08f046bcd7975b63d
Mateusz Wlekliński <matix0508@gmail.com>
2021-07-13 02:43:52 +0200

    an App

---
+++ move_files.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+from typing import List
+import inflect
+
+p = inflect.engine()
+
+
+class MoveApp:
+    def __init__(self, source: str, categories=None):
+        self.dir = source
+        self.categories = categories
+        self.target_dirs = []
+        self.make_target_dirs()
+
+    def make_target_dirs(self):
+        for category in self.categories:
+            self.target_dirs.append(os.path.join(self.dir, p.plural(category).capitalize()))
+
+    def remove_white_spaces(self) -> None:
+        for f in os.listdir(self.dir):
+            r = f.replace(" ","-")
+            if( r != f):
+                os.rename(os.path.join(self.dir, f),os.path.join(self.dir, r))
+
+    def get_orig(self, filename: str) -> str:
+        return os.path.join(self.dir, filename)
+
+    
+    def get_target(self, filename: str, target: str) -> str:
+        target_path = None
+        for i in range(len(self.categories)):
+            if target == self.categories[i]:
+                target_path = self.target_dirs[i]
+        if not target_path:
+            raise ValueError("I don't have anticipated that input LOL")
+
+        return os.path.join(target_path, filename)
+
+    def get_files(self) -> List[str]:
+        output = []
+        names = os.listdir(self.dir)
+        for name in names:
+            if os.path.isfile(os.path.join(self.dir, name)):
+                output.append(name)
+        return output
+
+    
+    def create_dirs(self) -> None:
+        for dir in self.target_dirs:
+            if not os.path.isdir(dir):
+                os.mkdir(dir)
+
+    def move_if(self, file: str, category: str) -> None:
+        if category in file.lower():
+            oryg = self.get_orig(file)
+            target = self.get_target(file, category)
+            shutil.move(oryg, target)
+
+    def move_files(self) -> None:
+        for file in self.get_files():
+            for category in self.categories:
+                self.move_if(file, category)
+
+    def run(self):
+        self.create_dirs()
+        self.remove_white_spaces()
+        self.move_files()
--- photos_files.py
+++ photos_files.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from typing import List
+from move_files import MoveApp
 
 
 PHOTO_DIR = '/home/wleklinski/Pictures'
@@ -12,64 +13,8 @@ TARGET_DIRS = [SCREENSHOTS_DIR, MEMES_DIR]
 CATEGORIES = ['screenshot', 'meme']
 
 
-def remove_white_spaces() -> None:
-    for f in os.listdir(PHOTO_DIR):
-        r = f.replace(" ","-")
-        if( r != f):
-            os.rename(os.path.join(PHOTO_DIR, f),os.path.join(PHOTO_DIR, r))
-
-
-
-def get_oryg(filename: str) -> str:
-    return os.path.join(PHOTO_DIR, filename)
-
-def get_target(filename: str, target: str) -> str:
-    if target == "meme":
-        target_path = MEMES_DIR
-    elif target == "screenshot":
-        target_path = SCREENSHOTS_DIR
-    else:
-        raise ValueError("I don't have anticipated that input LOL")
-
-    return os.path.join(target_path, filename)
-
-
-def get_files() -> List[str]:
-    output = []
-    names = os.listdir(PHOTO_DIR)
-    for name in names:
-        if os.path.isfile(os.path.join(PHOTO_DIR, name)):
-            output.append(name)
-    return output
-
-def create_dirs() -> None:
-    for dir in TARGET_DIRS:
-        if not os.path.isdir(dir):
-            os.mkdir(dir)
-
-def move_if(file: str, category: str) -> None:
-    # print(file)
-    # print(file.lower())
-    if category in file.lower():
-        oryg = get_oryg(file)
-        target = get_target(file, category)
-        print((oryg, target))
-        shutil.move(oryg, target)
-
-
-def move_files(files: List[str]) -> None:
-    for file in files:
-        for category in CATEGORIES:
-            move_if(file, category)
-
-def main() -> None:
-    create_dirs()
-    remove_white_spaces()
-    files = get_files()
-    # print(files)
-    move_files(files)
-
 if __name__ == "__main__":
-    main()
+    app = MoveApp(PHOTO_DIR, CATEGORIES)
+    app.run()
 
     
